067880a26151933c563430ef28979e9dc3385620
Ajitesh Kumar <ajinest6@gmail.com>
2026-03-20 01:25:04 -0400

    up down other style things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,48 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
+  var selectedButton = $state(0);
+
   let canvas: HTMLCanvasElement;
   onMount(() => {
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
+
+    var selectedButton = $state(0);
 
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
 
     var img = new Image();
     img.onload = () => {
-      ctx.drawImage(img, 20, 20, 350, 350);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.save();
+      ctx.translate(canvas.width / 5, canvas.height / 1.67);
+      ctx.rotate((Math.PI / 180) * 15);
+      ctx.drawImage(img, 20, 20, -img.width / 1.3, -img.height / 1.3);
+      ctx.restore();
     };
     img.src = "/images/arcademachine.png";
   });
+
+  document.onkeydown = (e) => {
+    console.log(e.key);
+    if (e.key === "ArrowDown") {
+      if (selectedButton >= 1) {
+        selectedButton = 0;
+        return;
+      }
+      selectedButton++;
+      console.log("selected button is,", selectedButton);
+    } else if (e.key === "ArrowUp") {
+      if (selectedButton <= 0) {
+        selectedButton = 0;
+        return;
+      }
+      selectedButton--;
+    }
+  };
+  console.log("changed selected button to, ", selectedButton);
 </script>
 
 <canvas class="w-full h-full absolute" id="bgCanvas" bind:this={canvas}></canvas>
@@ -41,11 +69,45 @@
     </h1>
     <p class="text-center text-2xl">make a game, get an <b class="text-3xl">arcade machine</b>!</p>
     <p class="text-center text-2xl">beginners always welcome!</p>
-    <div class="flex flex-row justify-center mt-2">
-      <button
-        class="bg-[#f2f9ff] border-2 border-[#338eda] text-[#338eda] hover:bg-[#338eda] hover:text-white duration-300 w-fit px-4 py-2 rounded-full"
-        >requirements</button
+    <div class="flex flex-col items-center justify-center mt-8 gap-3">
+      <button class="bg-[#f2f9ff] border-[#338eda] text-[#338eda] hover:bg-[#338eda]">
+        {#if selectedButton === 0}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-chevron-right-icon lucide-chevron-right"
+          >
+            <path d="m9 18 6-6-6-6" />
+          </svg>
+        {/if}
+        requirements</button
       >
+      <button class="bg-[#ffe4e8] border-[#ec3750] text-[#ec3750] hover:bg-[#ec3750]">
+        {#if selectedButton === 1}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-chevron-right-icon lucide-chevron-right"
+          >
+            <path d="m9 18 6-6-6-6" />
+          </svg>
+        {/if}
+        sign in to hack club
+      </button>
     </div>
   </div>
 </main>
